f2aee4fe6b8c50b2b6379ed23ebecfb2ad657562
Bayu Darmantra <darmantra@gmail.coma>
2023-11-11 23:18:12 +0800

    [WIP] store feed name & url in sync storage

--- js/feedsManagement.js
+++ js/feedsManagement.js
@@ -2,21 +2,52 @@ import { fetchFeeds } from './fetchFeeds.js';
 
 class FeedsManagement {
     constructor() {
+        this.sourceStoageName = 'sources';
         this.form = document.getElementById('form-add-feed');
 
         this.addFeed();
     }
 
     addFeed() {
-        this.form.addEventListener('submit', (e) => {
+        this.form.addEventListener('submit', e => {
             e.preventDefault();
 
             let formData = new FormData(this.form);
             const name = formData.get('feed_name');
             const url = formData.get('feed_url');
 
-            const feed = new fetchFeeds(url);
-            feed.fetchData();
+            //this.storeSource(name, url);
+
+            // const feed = new fetchFeeds(url);
+            // feed.fetchData();
+        });
+    }
+
+    storeSource( name, url ) {
+        chrome.storage.sync.get([this.sourceStoageName], (result) => {
+            let sources = [];
+
+            if( result[this.sourceStoageName] ) {
+                sources = result[this.sourceStoageName];
+            }
+
+            if( sources.length > 0 ) {
+                for( let i = 0; i < sources.length; i++ ) {
+                    if( sources[i].url === url ) {
+                        alert('Source already exists');
+                        return;
+                    }
+                }
+            }
+
+            sources.push({
+                name: name,
+                url: url
+            });
+
+            chrome.storage.sync.set({ sources: sources }, () => {
+                console.log('Source saved');
+            });
         });
     }
 }
